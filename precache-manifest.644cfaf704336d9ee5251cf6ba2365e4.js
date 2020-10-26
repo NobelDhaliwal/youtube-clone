eb5b5b6771c58bb12356fcef1f6a3e3d290dcdb8
nobel <nobelinderpreet@gmail.com>
2020-10-26 13:12:24 +1100

    Updates

---
+++ precache-manifest.644cfaf704336d9ee5251cf6ba2365e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6d826eb0f88280e6a97f6dc78c122c7",
+    "url": "/youtube-clone/index.html"
+  },
+  {
+    "revision": "a5657d95e019e7d5fecb",
+    "url": "/youtube-clone/static/css/main.e8b295b5.chunk.css"
+  },
+  {
+    "revision": "244899de52a42dd98992",
+    "url": "/youtube-clone/static/js/2.41f6b106.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/youtube-clone/static/js/2.41f6b106.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5657d95e019e7d5fecb",
+    "url": "/youtube-clone/static/js/main.5c5d9062.chunk.js"
+  },
+  {
+    "revision": "ae815b4c77d21c5f1fd8",
+    "url": "/youtube-clone/static/js/runtime-main.e3672875.js"
+  }
+]);
